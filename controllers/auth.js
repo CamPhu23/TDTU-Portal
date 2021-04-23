972cf224de0463f39eec0caf9f8be689ed36c3b9
Trí's Task <Ngthanhtri_3002>
2021-04-23 15:30:26 +0700

    View -> changePW, department List, notification, profile, register and style css

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,22 +1,22 @@
-const {OAuth2Client} = require('google-auth-library')
+const { OAuth2Client } = require('google-auth-library')
 const CLIENT_ID = "591004915054-1kjagbh4omn10rg7n36g8p7up4qrv058.apps.googleusercontent.com"
 const client = new OAuth2Client(CLIENT_ID)
 const userModel = require('../models/user')
 
-const {validationResult} = require('express-validator')
+const { validationResult } = require('express-validator')
 
 exports.getLogin = (req, res) => {
-    res.render('pages/login', {email: '', pass: ''})
+    res.render('pages/login', { email: '', pass: '' })
 }
 
 exports.postGoogleLogin = (req, res) => {
-    let {id_token} = req.body
+    let { id_token } = req.body
 
     let user = {}
     async function verify() {
         const ticket = await client.verifyIdToken({
             idToken: id_token,
-            audience: CLIENT_ID,  
+            audience: CLIENT_ID,
         });
         const payload = ticket.getPayload();
         const userid = payload['sub'];
@@ -25,35 +25,35 @@ exports.postGoogleLogin = (req, res) => {
         user.name = payload.name;
         user.picture = payload.picture;
     }
-    
+
     verify()
-    .then(() => {
-        domain = user.email.split("@")[1]
-    
-        if (domain === "student.tdtu.edu.vn") {
-            // res.cookie("session-token", id_token)
-            res.send({"result": "success"})
+        .then(() => {
+            domain = user.email.split("@")[1]
 
-                    
-            let account = userModel.findOne().
-                                    where(email).equals(email)
+            if (domain === "student.tdtu.edu.vn") {
+                // res.cookie("session-token", id_token)
+                res.send({ "result": "success" })
 
-            if (account.length === 0) {
-                user = new userModel(user.email, user.name + "123", user.name, user.picture, "3")
 
-                //Làm tời đây
-                user.save()
-                .then()
-                .catch()
+                // let account = userModel.findOne().
+                //                         where(email).equals(email)
+
+                // if (account.length === 0) {
+                //     user = new userModel(user.email, user.name + "123", user.name, user.picture, "3")
+
+                //     //Làm tời đây
+                //     user.save()
+                //     .then()
+                //     .catch()
+                // }
+            } else {
+                res.send({ "result": "Không thuộc tên miền student.tdtu.edu.vn" })
             }
-        } else {
-            res.send({"result": "Không thuộc tên miền student.tdtu.edu.vn"})
-        }
-    })
-    .catch((err) => {
-        console.log(err)
-        res.send({"result": err})
-    });
+        })
+        .catch((err) => {
+            console.log(err)
+            res.send({ "result": err })
+        });
 }
 
 exports.postLogin = (req, res) => {
@@ -62,14 +62,35 @@ exports.postLogin = (req, res) => {
         res.end(result.errors[0].msg)
     }
 
-    let {email, pass} = req.body
-    
+    let { email, pass } = req.body
+
     let account = userModel.findOne().
-                            where(email).equals(email)
+    where(email).equals(email)
 
     if (account.length !== 0 && account.password == pass) {
         res.send("Tồn tại tài khoản")
     }
 
     res.send("Bị sai tài khoản hoặc mật khẩu")
+}
+
+
+exports.getDepartmentlist = (req, res) => {
+    res.render('pages/departmentlist')
+}
+
+exports.getNotification = (req, res) => {
+    res.render('pages/notification')
+}
+
+exports.getProfile = (req, res) => {
+    res.render('pages/profile')
+}
+
+exports.getRegister = (req, res) => {
+    res.render('pages/register')
+}
+
+exports.getChangePassword = (req, res) => {
+    res.render('pages/changePassword')
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const authController = require('../controllers/auth')
-// const userModel = require('../models/user')
+    // const userModel = require('../models/user')
 
 const loginValidator = require('../middleware/loginValidator')
 
@@ -12,5 +12,15 @@ Router.get('/login', authController.getLogin)
 Router.post('/googlelogin', authController.postGoogleLogin)
 
 Router.post('/login', loginValidator, authController.postLogin)
+
+Router.get('/changePassword', authController.getChangePassword)
+
+Router.get('/departmentlist', authController.getDepartmentlist)
+
+Router.get('/notification', authController.getNotification)
+
+Router.get('/profile', authController.getProfile)
+
+Router.get('/register', authController.getRegister)
 
 module.exports = Router
