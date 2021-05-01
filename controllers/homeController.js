d3b808a8554cfafa8559cf77533d9a3a5e21f7e7
CamPhu23 <camphu480@gmail.com>
2021-05-01 17:21:09 +0700

    responsive and authorization

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -9,7 +9,7 @@ exports.showHomepage = async (req, res) => {
 
     let {userId, accountId} = req.session
     let permission = accountId ? true: false
-    authorization = await getAuthorization.getAuthorization(accountId)
+    authorization = getAuthorization.getAuthorization(accountId)
 
     User.findById(userId)
         .then(user => {
@@ -200,7 +200,7 @@ exports.handleShowPersonalProfile = async (req, res) => {
     let id = req.params.id
     let {userId, accountId} = req.session
 
-    authorization = await getAuthorization.getAuthorization(accountId)
+    authorization = getAuthorization.getAuthorization(accountId)
 
 
     console.log({userId, id});
